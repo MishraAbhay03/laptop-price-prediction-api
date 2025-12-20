0ae60a13c1263f3d59db898b7d9fd4085fb40d2c
Abhaykumar mishra <Mishra03abhaykumar@gmail.com>
2025-12-20 23:25:20 +0530

    Done

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests
 
-API_URL = "http://127.0.0.1:8000/predict"
+API_URL = "https://laptop-price-prediction-api-production.up.railway.app/"
 
 st.set_page_config(
     page_title="Laptop Price Predictor",
